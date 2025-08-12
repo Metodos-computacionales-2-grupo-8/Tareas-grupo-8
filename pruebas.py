7d7ecc0f53ded638a09d7f674e5b48fec2ee7913
Is-NotDark <j.pereze@uniandes.edu.co>
2025-08-11 22:46:11 -0500

    Punto 1 realizado?

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,10 @@
-for i in range(10, 51):
-    num = i
-    hola = "número{} equisde".format(num)
-    print(hola)
+import pandas as pd
+
+data = {'Category': ['A', 'B', 'A', 'C', 'B', 'A'],
+        'Value': [10, 15, 20, 5, 25, 30]}
+df = pd.DataFrame(data)
+
+# Group by 'Category' and calculate the sum of 'Value' for each category
+grouped_data = df.groupby('Category')['Value'].sum()
+
+print(grouped_data)
